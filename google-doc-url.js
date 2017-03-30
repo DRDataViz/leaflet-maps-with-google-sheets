65cd7a1a08db843d437f1804df8cf9eb6012717e
DRDataViz <dreichle@hamilton.edu>
2017-03-30 11:19:55 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o71Ps4FTod13SXAtatadm9F3nDrvCCNUHFj86VE58jA/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AoKRUBay4huDie9r_nffguRmFyiuZAucwCOQz1IMgDg/edit#gid=1805724235';
